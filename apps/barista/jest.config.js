151fd5d8127221c7a9dfcfb25d26b287b1ce32a0
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-03-05 08:48:58 +0100

    initial commit

---
+++ apps/barista/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'barista',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/barista',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
